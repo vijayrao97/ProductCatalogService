eb677da9e9a68c86c0f5ec07ae38102064a8edcd
vijayrao97 <vijayrao3113@gmail.com>
2024-10-06 10:30:13 +0530

    MOCK MVC test cases are added

--- src/test/java/com/scaler/productcatalogservice/controllers/ProductControllerMVCTest.java
+++ src/test/java/com/scaler/productcatalogservice/controllers/ProductControllerMVCTest.java
@@ -1,21 +1,25 @@
 package com.scaler.productcatalogservice.controllers;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.scaler.productcatalogservice.dtos.ProductDto;
 import com.scaler.productcatalogservice.models.Product;
 import com.scaler.productcatalogservice.services.IProductService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @WebMvcTest(ProductController.class)
 public class ProductControllerMVCTest {
@@ -46,11 +50,36 @@ public class ProductControllerMVCTest {
         when(productService.getAllProducts())
                 .thenReturn(productList);
 
-
-
         mockMvc.perform(get("/products"))
                 .andExpect(status().isOk())
-                .andExpect(content().string(objectMapper.writeValueAsString(productList)));
+                .andExpect(content().string(objectMapper.writeValueAsString(productList)))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].title").value("MacBookPro"))
+                .andExpect(jsonPath("$[1].id").value("2"));
+    }
+
+    @Test
+    public void Test_CreateProduct_RunSucessfully() throws Exception {
+        //Arrange
+        Product product = new Product();
+        product.setID(101L);
+        product.setTitle("Notebook");
+
+        ProductDto productDto = new ProductDto();
+        productDto.setID(101L);
+        productDto.setTitle("Notebook");
+
+        when(productService.createProduct(any(Product.class))).thenReturn(product);
+
+        //Act
+        mockMvc.perform(post("/products")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(productDto)))
+                .andExpect(status().isOk())
+                .andExpect(content().string(objectMapper.writeValueAsString(productDto)))
+                .andExpect(jsonPath("$.id").value(101L))
+                .andExpect(jsonPath("$.title").value("Notebook"));
+
     }
 
 }
--- src/test/java/com/scaler/productcatalogservice/controllers/ProductControllerTest.java
+++ src/test/java/com/scaler/productcatalogservice/controllers/ProductControllerTest.java
@@ -3,7 +3,10 @@ package com.scaler.productcatalogservice.controllers;
 import com.scaler.productcatalogservice.dtos.ProductDto;
 import com.scaler.productcatalogservice.models.Product;
 import com.scaler.productcatalogservice.services.IProductService;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
@@ -12,6 +15,7 @@ import org.springframework.http.ResponseEntity;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
@@ -22,6 +26,9 @@ class ProductControllerTest {
 
     @MockBean
     private IProductService productService;
+
+    @Captor
+    private ArgumentCaptor<Long> idCaptor;
 
     @Test
     public void Test_GetProductById_WithValidProductId_RunSuccessfully() {
@@ -46,6 +53,7 @@ class ProductControllerTest {
     }
 
     @Test
+    @DisplayName("passing id -1 lead to product not found")
     public void TestGetProductById_WithInvalid_ResultsInRuntimeException() {
         RuntimeException e =
         assertThrows(RuntimeException.class, () -> productController.getProductById(-1L));
@@ -59,6 +67,23 @@ class ProductControllerTest {
 
         assertThrows(RuntimeException.class,
                 () -> productController.getProductById(1000L));
+    }
+
+    @Test
+    public void Test_getProductByID_ServiceCalledWithExpectedArguments_RunSuccessfully() {
+        //Arrange
+        Long productId = 1L;
+        Product product = new Product();
+        product.setID(productId);
+        product.setTitle("Nokia");
+        when(productService.getProductById(any(Long.class)))
+                .thenReturn(product);
+        // Act
+        productController.getProductById(productId);
+
+        //Assert
+        verify(productService).getProductById(idCaptor.capture());
+        assertEquals(productId, idCaptor.getValue());
     }
 
 }
